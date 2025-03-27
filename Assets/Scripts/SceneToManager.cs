ed89ea987bb81176f46efc8e1c455c6f1fbcb102
Nathuit <natheight8@gmail.com>
2025-03-27 22:58:22 +0100

    ballon-pas-fix-explosion

--- Assets/Scripts/SceneToManager.cs
+++ Assets/Scripts/SceneToManager.cs
@@ -4,9 +4,17 @@ using UnityEngine.InputSystem;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using TMPro;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
 
 public class SceneToManager : MonoBehaviour
 {
+    public GameObject ParticleEffect;
+    private Vector2 touchPos;
+    private RaycastHit hit;
+    private Camera cam;
+    
+    //public PlayerInput playerInput;
     public ARRaycastManager RaycastManager;
     public TrackableType TypeToTrack = TrackableType.PlaneWithinBounds;
     public GameObject PrefabToInstantiate;
@@ -18,27 +26,34 @@ public class SceneToManager : MonoBehaviour
     private InputAction touchPhaseAction;
     [SerializeField] private TMP_Text countText;
     [SerializeField] private TMP_Text timer;
-    private float _timer;
+    [SerializeField] double _timer = 30;
     private int cubeCount;
+    private bool GameStarted = false;
+    [SerializeField] double SpawnDelay;
+    private double _spawnDelay = 0;
+    private ARRaycastHit firstHit;
+    public float SpawnRange = 3f;
+
     private void OnTouch()
     {
-        var touchPos = touchPosAction.ReadValue<Vector2>();
+        touchPos = touchPosAction.ReadValue<Vector2>();
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         RaycastManager.Raycast(touchPos, hits, TypeToTrack);
         if (hits.Count > 0)
         {
-            ARRaycastHit firstHit = hits[0];
-            GameObject cube = Instantiate(PrefabToInstantiate, firstHit.pose.position, firstHit.pose.rotation);
-            instantiatedCubes.Add(cube);
-            cubeCount += 1;
-            countText.text = "Cubes: " + cubeCount;
-            _timer += Time.deltaTime;
-            timer.text = $"{_timer}";
+            firstHit = hits[0];
+            GameObject ballon = Instantiate(PrefabToInstantiate, firstHit.pose.position, firstHit.pose.rotation);
+            int randomIndex = Random.Range(0, Materials.Count);
+            Material randomMaterial = Materials[randomIndex];
+            ballon.GetComponent<MeshRenderer>().material = randomMaterial;
+            instantiatedCubes.Add(ballon);
+            GameStarted = true;
         }
     }    
     // Start is called before the first frame update
     void Start()
     {
+        cam = GetComponent<Camera>();
         touchPressAction = PlayerInput.actions["TouchPress"];
         touchPosAction = PlayerInput.actions["TouchPos"];
         touchPhaseAction = PlayerInput.actions["TouchPhase"];
@@ -51,11 +66,58 @@ public class SceneToManager : MonoBehaviour
     {
         if (touchPressAction.WasPerformedThisFrame())
         {
-            var touchPhase = touchPhaseAction.ReadValue<UnityEngine.InputSystem.TouchPhase>();
-            if (touchPhase == UnityEngine.InputSystem.TouchPhase.Began)
+            if (GameStarted)
             {
-                OnTouch();
+                touchPos = touchPosAction.ReadValue<Vector2>();
+                //countText.text = $"test : {cubeCount}";
+
+                Ray ray = cam.ScreenPointToRay(touchPos);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    countText.text = $"test 2 : {cubeCount}";
+
+                    GameObject hitObj = hit.collider.gameObject;
+                    if (hitObj.tag == "Enemy")
+                    {
+                        var clone = Instantiate(ParticleEffect, hitObj.transform.position, Quaternion.identity);
+                        clone.transform.localScale = hitObj.transform.localScale;
+                        cubeCount++;
+                        countText.text = $"counter : {cubeCount}";
+                        Destroy(hitObj);
+                    }
+                }
             }
+            else
+            {
+                var touchPhase = touchPhaseAction.ReadValue<UnityEngine.InputSystem.TouchPhase>();
+                if (touchPhase == UnityEngine.InputSystem.TouchPhase.Began)
+                {
+                    OnTouch();
+                }
+            }
+            
+        }
+
+        if (GameStarted)
+        {
+            if (_spawnDelay < 0)
+            {
+                _spawnDelay = SpawnDelay;
+                float x = firstHit.pose.position.x+ Random.Range(-SpawnRange, SpawnRange);
+                float y = firstHit.pose.position.y;
+                float z = firstHit.pose.position.z;
+                Vector3 spawnPos = new Vector3(x, y, z);
+                GameObject ballon = Instantiate(PrefabToInstantiate, spawnPos, firstHit.pose.rotation);
+                int randomIndex = Random.Range(0, Materials.Count);
+                Material randomMaterial = Materials[randomIndex];
+                ballon.GetComponent<MeshRenderer>().material = randomMaterial;
+                instantiatedCubes.Add(ballon);
+            }
+
+            _spawnDelay -= Time.deltaTime;
+
+            _timer -= Time.deltaTime;
+            timer.text = $"{math.round(_timer * 100) / 100}";
         }
     }
     
